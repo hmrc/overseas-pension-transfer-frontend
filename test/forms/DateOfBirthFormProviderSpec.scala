1e13428adae4060dea5f320ea703cd033568a4f2
Robert Rowney <37312825+BritishKid@users.noreply.github.com>
2025-01-28 13:49:17 +0000

    Generated Date Of Birth page

--- test/forms/DateOfBirthFormProviderSpec.scala
+++ test/forms/DateOfBirthFormProviderSpec.scala
@@ -22,8 +22,6 @@ import play.api.data.FormError
 import play.api.i18n.Messages
 import play.api.test.Helpers.stubMessages
 
-import java.time.format.DateTimeFormatter
-
 class DateOfBirthFormProviderSpec extends DateBehaviours {
 
   private implicit val messages: Messages = stubMessages()
