39f4c8bb77d33c0bf24062b41d5136001ed96f75
Mathew May <29102292+mghmay@users.noreply.github.com>
2025-04-10 11:42:37 +0100

    [OAOTC-890] continue rebasing

--- app/connectors/UserAnswersConnector.scala
+++ app/connectors/UserAnswersConnector.scala
@@ -1,14 +1,14 @@
 /*
  * Copyright 2023 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the \"License\");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an \"AS IS\" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,6 +17,7 @@
 package connectors
 
 import config.FrontendAppConfig
+import models.dtos.UserAnswersDTO
 import play.api.Logging
 import play.api.libs.json.{JsValue, Json}
 import uk.gov.hmrc.http.HttpReads.Implicits.readJsValue
@@ -29,8 +30,8 @@ import scala.concurrent.{ExecutionContext, Future}
 
 sealed trait UserAnswersResponse
 
-case class UserAnswersSuccessResponse(json: JsValue)  extends UserAnswersResponse
-case class UserAnswersErrorResponse(cause: Throwable) extends UserAnswersResponse
+case class UserAnswersSuccessResponse(userAnswersDTO: UserAnswersDTO) extends UserAnswersResponse
+case class UserAnswersErrorResponse(cause: Throwable)                 extends UserAnswersResponse
 
 class UserAnswersConnector @Inject() (
     appConfig: FrontendAppConfig,
@@ -42,9 +43,9 @@ class UserAnswersConnector @Inject() (
 
   def getAnswers(id: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[UserAnswersResponse] = {
     http.get(userAnswersUrl(id))
-      .execute[JsValue]
-      .map { json =>
-        UserAnswersSuccessResponse(json)
+      .execute[UserAnswersDTO]
+      .map { uad =>
+        UserAnswersSuccessResponse(uad)
       }
       .recover {
         case e: Exception =>
@@ -53,12 +54,17 @@ class UserAnswersConnector @Inject() (
       }
   }
 
-  def putAnswers(id: String, body: JsValue)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[UserAnswersResponse] = {
+  def putAnswers(
+      id: String,
+      userAnswersDTO: UserAnswersDTO
+    )(implicit hc: HeaderCarrier,
+      ec: ExecutionContext
+    ): Future[UserAnswersResponse] = {
     http.put(userAnswersUrl(id))
-      .withBody(body)
-      .execute[JsValue]
-      .map { updatedJson =>
-        UserAnswersSuccessResponse(updatedJson)
+      .withBody(Json.toJson(userAnswersDTO))
+      .execute[UserAnswersDTO]
+      .map { updatedDto =>
+        UserAnswersSuccessResponse(updatedDto)
       }
       .recover {
         case e: Exception =>
@@ -66,4 +72,5 @@ class UserAnswersConnector @Inject() (
           UserAnswersErrorResponse(e)
       }
   }
+
 }
