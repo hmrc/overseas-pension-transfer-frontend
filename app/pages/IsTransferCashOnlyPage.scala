2613bb2359ab4493a35a3a8213f9f51be27ed095
Miraj Khan <189656860+mKhan-838919@users.noreply.github.com>
2025-05-06 14:35:37 +0100

    OAOTC-762 Refactored file location

--- app/pages/IsTransferCashOnlyPage.scala
+++ app/pages/IsTransferCashOnlyPage.scala
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package pages
 
 import controllers.routes
-import models.UserAnswers
+import models.{CheckMode, UserAnswers}
 import play.api.libs.json.JsPath
 import play.api.mvc.Call
 
@@ -12,5 +28,11 @@ case object IsTransferCashOnlyPage extends QuestionPage[Boolean] {
   override def toString: String = "isTransferCashOnly"
 
   override protected def nextPageNormalMode(answers: UserAnswers): Call =
-    routes.IndexController.onPageLoad()
+    routes.IndexController.onPageLoad() // TODO change while connecting the pages
+
+  override protected def nextPageCheckMode(answers: UserAnswers): Call =
+    routes.TransferDetailsCYAController.onPageLoad()
+
+  final def changeLink(answers: UserAnswers): Call =
+    routes.IsTransferCashOnlyController.onPageLoad(CheckMode)
 }
--- test/pages/IsTransferCashOnlyPageSpec.scala
+++ test/pages/IsTransferCashOnlyPageSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package pages
 
 import controllers.routes
@@ -14,7 +30,7 @@ class IsTransferCashOnlyPageSpec extends AnyFreeSpec with Matchers {
     "in Normal Mode" - {
 
       "must go to Index" in {
-
+        // TODO will need to be changed once pages are connected
         IsTransferCashOnlyPage.nextPage(NormalMode, emptyAnswers) mustEqual routes.IndexController.onPageLoad()
       }
     }
@@ -23,7 +39,7 @@ class IsTransferCashOnlyPageSpec extends AnyFreeSpec with Matchers {
 
       "must go to Check Answers" in {
 
-        IsTransferCashOnlyPage.nextPage(CheckMode, emptyAnswers) mustEqual routes.CheckYourAnswersController.onPageLoad()
+        IsTransferCashOnlyPage.nextPage(CheckMode, emptyAnswers) mustEqual routes.TransferDetailsCYAController.onPageLoad()
       }
     }
   }
