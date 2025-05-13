8eb624755af9504ede40705117934ce26945dc6c
Miraj Khan <189656860+mKhan-838919@users.noreply.github.com>
2025-05-13 10:30:37 +0100

    OAOTC-825 Fixed build failure

--- test-utils/generators/ModelGenerators.scala
+++ test-utils/generators/ModelGenerators.scala
@@ -26,7 +26,7 @@ trait ModelGenerators {
     Arbitrary {
       Gen.oneOf(TypeOfAsset.values)
     }
-  
+
   implicit lazy val arbitraryApplicableTaxExclusions: Arbitrary[ApplicableTaxExclusions] =
     Arbitrary {
       Gen.oneOf(ApplicableTaxExclusions.values)
