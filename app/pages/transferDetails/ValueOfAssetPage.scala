e0e11a115a770b63d381175ab8346af6c4ba1c13
Miraj Khan <189656860+mKhan-838919@users.noreply.github.com>
2025-06-17 14:26:52 +0100

    OAOTC-1045 Added corrected JSPath of 'transferDetails'

--- app/pages/transferDetails/ValueOfAssetPage.scala
+++ app/pages/transferDetails/ValueOfAssetPage.scala
@@ -17,14 +17,14 @@
 package pages.transferDetails
 
 import controllers.transferDetails.routes
-import models.{CheckMode, UserAnswers}
+import models.{CheckMode, TaskCategory, UserAnswers}
 import pages.QuestionPage
 import play.api.libs.json.JsPath
 import play.api.mvc.Call
 
 case object ValueOfAssetPage extends QuestionPage[BigDecimal] {
 
-  override def path: JsPath = JsPath \ toString
+  override def path: JsPath = JsPath \ TaskCategory.TransferDetails.toString \ toString
 
   override def toString: String = "valueOfAsset"
 
