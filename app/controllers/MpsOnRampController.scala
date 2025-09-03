7244f9755d8e0d3433c626c0eccb570ee67b0d6d
Mathew May <29102292+mghmay@users.noreply.github.com>
2025-09-03 12:03:15 +0100

    OAOTC-1317 Add MPS on ramp controller alongside dashboard repo

---
+++ app/controllers/MpsOnRampController.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.IdentifierAction
+import models.DashboardData
+import pages.MpsOnRampPage
+
+import javax.inject._
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import play.api.i18n.I18nSupport
+import queries.{PstrQuery, ReturnUrlQuery}
+import repositories.DashboardSessionRepository
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class MpsOnRampController @Inject()(
+    val controllerComponents: MessagesControllerComponents,
+    repo: DashboardSessionRepository,
+    identify: IdentifierAction
+  )(implicit ec: ExecutionContext
+  ) extends FrontendBaseController with I18nSupport {
+
+  def onRamp(pstr: String, returnUrl: String): Action[AnyContent] = identify.async { implicit request =>
+    for {
+      dashboardData <- Future.fromTry(new DashboardData(request.authenticatedUser.internalId).set(PstrQuery, pstr))
+      dd1           <- Future.fromTry(dashboardData.set(ReturnUrlQuery, returnUrl))
+      _             <- repo.set(dd1)
+    } yield Redirect(MpsOnRampPage.nextPage(dd1))
+  }
+}
---
+++ app/pages/MpsOnRampPage.scala
@@ -0,0 +1,9 @@
+package pages
+
+import models.DashboardData
+import play.api.mvc.Call
+
+object MpsOnRampPage extends Page {
+  def nextPage(data: DashboardData): Call = ???
+//    routes.DashboardController.onPageLoad()
+}
