e16433dd2e19adfd7cdabf963aae4cd14142bb3d
Tateni93 <33066342+Tateni93@users.noreply.github.com>
2025-09-03 15:09:42 +0100

    OAOTC-1450: Added headers to association check

--- app/connectors/PensionSchemeConnector.scala
+++ app/connectors/PensionSchemeConnector.scala
@@ -2,8 +2,9 @@ package connectors
 
 import com.google.inject.Inject
 import config.FrontendAppConfig
-import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
+import models.authentication.PsaId
 import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
 
 import java.net.URL
 import scala.concurrent.ExecutionContext
@@ -14,11 +15,22 @@ class PensionSchemeConnector @Inject()(
                                         http: HttpClientV2
                                       )(implicit ec: ExecutionContext) {
 
-  def getScheme(srn: String)(implicit hc: HeaderCarrier) = {
+  def getScheme()(implicit hc: HeaderCarrier) = {
     val url: URL = url"${appConfig.pensionSchemeService}/scheme"
 
     http.get(url)
-      .execute[HttpResponse]
+      .execute
+  }
+
+  def checkAssociation(srn: String, psaId: PsaId)(implicit hc: HeaderCarrier) = {
+    val url = url"${appConfig.pensionSchemeService}/register-scheme"
+
+    http.post(url)
+      .setHeader(
+        "schemeReferenceNumber" -> srn,
+        "psaId" -> psaId.value
+      )
+      .execute
   }
 
 }
