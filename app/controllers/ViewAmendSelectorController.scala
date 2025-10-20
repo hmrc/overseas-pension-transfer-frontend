14deef661519b4a35b464e56eec0c3aaa7a731c6
Bradleigh-L <189644673+Bradleigh-L@users.noreply.github.com>
2025-10-20 14:59:58 +0100

    OATC-1325: Create a new radio button page and implement routing for viewing a submitted return

---
+++ app/controllers/ViewAmendSelectorController.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import models.{PstrNumber, QtStatus}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.ViewAmendSelectorView
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class ViewAmendSelectorController @Inject() (
+    override val messagesApi: MessagesApi,
+    identify: IdentifierAction,
+    getData: DataRetrievalAction,
+    schemeData: SchemeDataAction,
+    val controllerComponents: MessagesControllerComponents,
+    view: ViewAmendSelectorView
+  )(implicit ec: ExecutionContext
+  ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(qtReference: String, pstr: PstrNumber, qtStatus: QtStatus, versionNumber: String): Action[AnyContent] =
+    (identify andThen schemeData andThen getData) { implicit request =>
+      Ok(view(qtReference, pstr, qtStatus, versionNumber)).withSession(
+        request.session +
+          ("qtReference"   -> qtReference) +
+          ("pstr"          -> pstr.value) +
+          ("qtStatus"      -> qtStatus.toString) +
+          ("versionNumber" -> versionNumber)
+      )
+    }
+
+  def onSubmit(qtReference: String, pstr: PstrNumber, qtStatus: QtStatus, versionNumber: String): Action[AnyContent] =
+    (identify andThen schemeData andThen getData).async { implicit request =>
+      for {
+        formData <- Future.successful(
+          request.body.asFormUrlEncoded
+            .flatMap(_.get("option").flatMap(_.headOption))
+        )
+        result   <- formData match {
+          case Some("view") =>
+            Future.successful(Redirect(routes.ViewSubmittedController.fromDashboard(qtReference, pstr, qtStatus, versionNumber)))
+//TODO - implement routing for amend functionality
+          case Some("amend") =>
+            Future.successful(Redirect(controllers.routes.TaskListController.onPageLoad()))
+
+          case _ =>
+            Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+        }
+      } yield result
+    }
+}
