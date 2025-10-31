8bd778243d7dfe8bc9d8aab0140a1a901656b127
Bradleigh-L <189644673+Bradleigh-L@users.noreply.github.com>
2025-10-31 16:21:29 +0000

    OAOTC-1636: The PSA/PSP must not be able to amend the date of transfer to a date after the submission date of version 001 of the APSS262

---
+++ app/forms/transferDetails/AmendDateOfTransferFormProvider.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.transferDetails
+
+import forms.mappings.Mappings
+import play.api.data.Form
+import play.api.i18n.Messages
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import javax.inject.Inject
+
+class AmendDateOfTransferFormProvider @Inject() extends Mappings {
+
+  def minDate: LocalDate = LocalDate of (2012, 4, 6)
+
+  private def dateFormatter = DateTimeFormatter.ofPattern("dd MM yyyy")
+
+  def apply(submissionDate: LocalDate)(implicit messages: Messages): Form[LocalDate] = {
+    val dayBeforeSubmission = submissionDate.minusDays(1)
+
+    Form(
+      "value" -> localDate(
+        invalidCharacter = "common.dateInput.error.invalid.character",
+        invalidKey       = "dateOfTransfer.error.invalid",
+        requiredKey      = "common.dateInput.error.required",
+        twoRequiredKey   = "common.dateInput.error.required.two",
+        allRequiredKey   = "dateOfTransfer.error.required.all"
+      )
+        .verifying(
+          minDate(minDate, "dateOfTransfer.error.invalid.mindate", minDate.format(dateFormatter)),
+          maxDate(dayBeforeSubmission, "dateOfTransfer.amend.error.afterSubmissionDate", submissionDate.format(dateFormatter))
+        )
+    )
+  }
+}
