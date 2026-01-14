d2becf1c8e9af988717eda1f33aff66eb43b7365
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-01-14 10:47:18 +0000

    OAOTC-1569 Add test for email sent text

--- app/controllers/PspDeclarationController.scala
+++ app/controllers/PspDeclarationController.scala
@@ -64,34 +64,34 @@ class PspDeclarationController @Inject() (
           Future.successful(BadRequest(view(formWithErrors, mode))),
         psaIdString => {
           val psaId = PsaId(psaIdString)
-          userAnswersService.submitDeclaration(request.authenticatedUser, request.userAnswers, request.sessionData, Some(psaId))
-            .flatMap {
-              case Right(SubmissionResponse(qtNumber, receiptDate)) =>
-                (for {
-                  updateWithQTNumberSD    <- EitherT.right[Result](Future.fromTry(request.sessionData.set(QtNumberQuery, qtNumber)))
-                  updateWithReceiptDateSD <- EitherT.right[Result](Future.fromTry(updateWithQTNumberSD.set(DateSubmittedQuery, receiptDate)))
-                  name                     = request.sessionData.get(MemberNamePage)
-                                               .orElse(request.userAnswers.get(MemberNamePage))
-                                               .getOrElse(PersonName("Undefined", "Undefined"))
-                  updateWithMemberNameSD  <- EitherT.right[Result](Future.fromTry(updateWithReceiptDateSD.set(MemberNamePage, name)))
-                  pspId                    = request.authenticatedUser.asInstanceOf[PspUser].pspId
-                  minimalDetails          <- EitherT(minimalDetailsConnector.fetch(pspId)).leftMap { e =>
-                                               logger.warn(s"[PspDeclarationController][onSubmit] Failed to fetch minimal details for pspId=${pspId.value}: $e")
-                                               Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())
-                                             }
-                  _                       <- EitherT.right[Result](
-                                               emailService
-                                                 .sendConfirmationEmail(updateWithMemberNameSD, minimalDetails)
-                                                 .map(_ => ())
-                                             )
-                } yield Redirect(PspDeclarationPage.nextPage(mode, request.userAnswers))).merge
-              case Left(NotAuthorisingPsaIdErrorResponse(_, _))     =>
-                val formWithError = form.withError("value", "pspDeclaration.error.notAuthorisingPsaId")
-                Future.successful(BadRequest(view(formWithError, mode)))
-              case e                                                =>
-                logger.warn(s"[PspDeclarationController][onSubmit] Failed to submit declaration: $e")
-                Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
-            }
+          (for {
+            submissionResponse      <-
+              EitherT(userAnswersService.submitDeclaration(request.authenticatedUser, request.userAnswers, request.sessionData, Some(psaId))).leftMap {
+                case NotAuthorisingPsaIdErrorResponse(_, _) =>
+                  val formWithError = form.withError("value", "pspDeclaration.error.notAuthorisingPsaId")
+                  BadRequest(view(formWithError, mode))
+                case e                                      =>
+                  logger.warn(s"[PspDeclarationController][onSubmit] Failed to submit declaration: $e")
+                  Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())
+              }
+            updateWithQTNumberSD    <- EitherT.right[Result](Future.fromTry(request.sessionData.set(QtNumberQuery, submissionResponse.qtNumber)))
+            updateWithReceiptDateSD <- EitherT.right[Result](Future.fromTry(updateWithQTNumberSD.set(DateSubmittedQuery, submissionResponse.receiptDate)))
+            name                     = request.sessionData.get(MemberNamePage)
+                                         .orElse(request.userAnswers.get(MemberNamePage))
+                                         .getOrElse(PersonName("Undefined", "Undefined"))
+            updateWithMemberNameSD  <- EitherT.right[Result](Future.fromTry(updateWithReceiptDateSD.set(MemberNamePage, name)))
+            pspId                    = request.authenticatedUser.asInstanceOf[PspUser].pspId
+            minimalDetails          <- EitherT(minimalDetailsConnector.fetch(pspId)).leftMap { e =>
+                                         logger.warn(s"[PspDeclarationController][onSubmit] Failed to fetch minimal details for pspId=${pspId.value}: $e")
+                                         Redirect(controllers.routes.JourneyRecoveryController.onPageLoad())
+                                       }
+            _                       <- EitherT.right[Result](
+                                         // Currently we do nothing with the return value from the email service. If we want to map the error we can do so here.
+                                         emailService
+                                           .sendConfirmationEmail(updateWithMemberNameSD, minimalDetails)
+                                           .map(_ => ())
+                                       )
+          } yield Redirect(PspDeclarationPage.nextPage(mode, request.userAnswers))).merge
         }
       )
   }
