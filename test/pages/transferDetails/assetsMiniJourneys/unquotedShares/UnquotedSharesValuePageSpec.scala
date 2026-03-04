a177666ef8f3162e304e94b4576f5e3031f81229
tharunprasath <65491423+tharunprasath@users.noreply.github.com>
2026-03-04 16:05:27 +0000

    OAOTC-2060 Unit test

--- test/pages/transferDetails/assetsMiniJourneys/unquotedShares/UnquotedSharesValuePageSpec.scala
+++ test/pages/transferDetails/assetsMiniJourneys/unquotedShares/UnquotedSharesValuePageSpec.scala
@@ -18,10 +18,10 @@ package pages.transferDetails.assetsMiniJourneys.unquotedShares
 
 import base.SpecBase
 import controllers.transferDetails.assetsMiniJourneys.AssetsMiniJourneysRoutes
+import models.assets.TypeOfAsset
 import models.{AmendCheckMode, CheckMode, FinalCheckMode, NormalMode, PstrNumber, UserAnswers}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
-import pages.transferDetails.assetsMiniJourneys.quotedShares.QuotedSharesValuePage
 
 class UnquotedSharesValuePageSpec extends AnyFreeSpec with Matchers with SpecBase {
   private val index = 0
@@ -64,6 +64,16 @@ class UnquotedSharesValuePageSpec extends AnyFreeSpec with Matchers with SpecBas
           index
         )
       }
+    }
+
+    "should go to CYA when complete" in {
+      val ua = emptyUserAnswers.copy(data = completeJson(TypeOfAsset.UnquotedShares))
+      UnquotedSharesValuePage(index).nextPage(NormalMode, ua) mustBe AssetsMiniJourneysRoutes.UnquotedSharesCYAController.onPageLoad(NormalMode, index)
+    }
+
+    "should go to Shares Number page when incomplete" in {
+      val ua = emptyUserAnswers.copy(data = incompleteJson())
+      UnquotedSharesValuePage(index).nextPage(NormalMode, ua) mustBe AssetsMiniJourneysRoutes.UnquotedSharesNumberController.onPageLoad(NormalMode, index)
     }
   }
 }
