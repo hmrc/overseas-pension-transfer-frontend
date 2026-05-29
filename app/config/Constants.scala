bd34dbd1edd7195db7461cb2bbcf8d0d11922e1f
joecefnhedog <7061639+joecefnhedog@users.noreply.github.com>
2026-05-29 15:00:53 +0100

    DLSN-514: Use shared constants for repeated values

---
+++ app/config/Constants.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import java.time.format.DateTimeFormatter
+
+object Constants {
+
+  val maxCurrency = 999999999.99
+  val minCurrency = 0.01
+
+  val ddMMyyyy: DateTimeFormatter  = DateTimeFormatter.ofPattern("dd MM yyyy")
+  val dMMMMyyyy: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy")
+
+}
